4efa73a8d5d6c69e80aabab162322e3946579f1e
ec0662sr <Ramona05!>
2016-02-18 18:06:17 -0600

    added dinosaur

--- DinosaurSupervisor.java
+++ DinosaurSupervisor.java
@@ -6,9 +6,11 @@ public class DinosaurSupervisor {
                 Dinosaur t_rex = new Dinosaur();
                 Dinosaur velociraptor = new Dinosaur();
                 Dinosaur diplodocus = new Dinosaur();
+                Dinosaur another = new Dinosaur();
 
                 t_rex.roar();
                 velociraptor.roar();
                 diplodocus.roar();
+                another.roar();
     }
 }
